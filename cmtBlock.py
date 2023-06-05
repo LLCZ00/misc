9be6fe92ac947fbced91e32eb33b674e927da9af
Clarence <37959893+LLCZ00@users.noreply.github.com>
2023-06-05 14:15:15 -0400

    Update cmtBlock.py

--- cmtBlock.py
+++ cmtBlock.py
@@ -1,60 +1,102 @@
 #!/bin/python3
+#
+# Copyright (C) 2023 LLCZ00
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.  
+#
 
-_NAME = "cmtBlock.py"
-_VERS = "1.0"
-_AUTHOR = "LLCZ00"
+"""
+Comment Block Generator
+"""
 
 import sys
+import argparse
+
+_NAME = "cmtblock.py"
+_VERSION = "1.1.0"
+_AUTHOR = "LLCZ00"
+_DESCRIPTION = f"""{_NAME} {_VERSION}, by {_AUTHOR}
+Script/tool for creating comment 'block' in desired language's style.
+Basedd purely on my own preferences.
+"""
+
+class CMTParser(argparse.ArgumentParser):
+    """Override argparse class for better error handler"""
+    def error(self, message="Unknown error", help=False):
+        if help:
+            self.print_help()
+        else:
+            print(f"Error. {message}")
+            print(f"Try './{self.prog} --help' for more information.")
+        sys.exit(1)
 
 
-def help():
-    print(
-        """{0} {1}, by {2}
-
-Description: Outputs the given string surrounded by the given/default character, 
-for use as comments in whatever language accepts that character as a comment.
-Default Character - #
-Purly stylistic, based on my own preferences. 
-
-Usage: ./{0} \"YOUR COMMENT\" [character]
-
-Examples:
-./{0} \"Surround with the default hash symbol\"
-./{0} \"Surround with x's\" x
-./{0} \"Surround with special symbol, stars\" \"*\"""".format(
-            _NAME, _VERS, _AUTHOR
-        )
+def parseArguments():
+    parser = CMTParser(
+    prog=_NAME,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=_DESCRIPTION,
+    epilog=f"Examples:\n\t{_NAME} comment\n\t{_NAME} --style c \"C-styled comment block\""
     )
-    sys.exit(0)
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version=f'{_NAME} v{_VERSION}',
+        help='Show version number and exit'
+    )
+    parser.add_argument(
+        '--style', 
+        choices=["python", "c"],
+        dest='style',
+        type=str.lower,
+        default="python",
+        help='Language style to return comment block in (Case insensitive).'
+    )
+    parser.add_argument(
+        'comment',
+        help="Comment to 'blockify'"
+    )
+    return parser.parse_args()
 
 
-def make_comment_block(comment, char="#"):
-    comment = "{} {} {}".format(char, comment, char)
-    print("{0}\n{1}\n{0}".format(char * len(comment), comment))
+class Comment:
+    def __init__(self, content):
+        self.content = content
+
+    def __str__(self):
+        return str(self.content)
+
+    def __style_python(self):
+        cmt = f"# {self.content} #"
+        bookend = '#' * len(cmt)
+        return f"{bookend}\n{cmt}\n{bookend}"
+
+    def __style_c(self):
+        cmt = f"* {self.content} *"
+        bookend_top = f"/*{'*' * (len(cmt) - 2)}"
+        bookend_bottom = f"{'*' * (len(cmt) - 2)}*/"
+        return f"{bookend_top}\n{cmt}\n{bookend_bottom}"
+
+    def __format__(self, style="python"):
+        if style == "python":
+            return self.__style_python()
+        elif style == "c":
+            return self.__style_c()
+        else:
+            return self.content
 
 
 def main():
-    try:
-
-        comment = sys.argv[1]
-        if comment == "--help":
-            help()
-
-    except IndexError:
-        print(
-            "Error: Missing 'COMMENT'\nTry '{} --help' for more information.".format(
-                _NAME
-            )
-        )
-        sys.exit(1)
-
-    try:
-        char = sys.argv[2]
-    except IndexError:
-        make_comment_block(comment)  # Default
-    else:
-        make_comment_block(comment, char=char)  # Custom Character
+    args = parseArguments()
+    comment = Comment(args.comment)
+    print(format(comment, args.style))
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
